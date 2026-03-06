70b6490f4c6b89715ca709894c2eb856d32d35b5
aman-maurya19 <amanmaurya12yy@gmail.com>
2026-03-06 17:11:47 +0530

    Fix: Unified Application module and resolved syntax errors

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,22 +1,22 @@
 package com.example
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
-
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
-
 }
 
-fun Application.module(configureSerialization: () -> Unit) {
-    DatabaseFactory.init() // Ye database connect karega
-    configureSerialization()
 fun Application.module() {
+    // Database initialize karein
+    DatabaseFactory.init()
 
+    // JSON serialization setup karein
     install(ContentNegotiation) {
         json()
     }
 
+    // Routing setup karein
     configureRouting()
 }
-
